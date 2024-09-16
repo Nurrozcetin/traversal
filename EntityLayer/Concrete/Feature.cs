03415aa7174dc9803ce7c4bfb6fb96bd23044d15
Fatmanur Ozcetin <nurozcetin35@gmail.com>
2024-09-16 19:07:09 +0300

    initial commit

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Feature
+	{
+        public int FeatureID { get; set; }
+        public string PostName { get; set; }
+        public string PostDescription { get; set; }
+        public string PostImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
