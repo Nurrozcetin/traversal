935bade4ade952342908d98b402a7bcc50e53020
Fatmanur Ozcetin <nurozcetin35@gmail.com>
2024-10-14 14:33:42 +0300

    initial comment

---
+++ DataAccessLayer/EntityFramework/EFFeature2Dal.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+	public class EFFeature2Dal : GenericRepository<Feature>, IFeature2Dal
+	{
+		public void Delete(Feature2 t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(Feature2 t)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Feature2 t)
+		{
+			throw new NotImplementedException();
+		}
+
+		List<Feature2> IGenericDal<Feature2>.GetList()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ProjectTraversal/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTraversal.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
